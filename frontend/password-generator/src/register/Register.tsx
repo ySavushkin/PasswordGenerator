ba7ac86d80a23523137aa103bd967dcbac9a5553
Nastik-14 <Anastasiya.14.trambo@icoud.com>
2025-04-01 23:47:52 +0300

    [frontend] Added Confirm Password field

--- frontend/password-generator/src/register/Register.tsx
+++ frontend/password-generator/src/register/Register.tsx
@@ -3,21 +3,32 @@ import React from "react";
 const RegistrationForm: React.FC = () => {
   return (
     <div className="bd-example">
+        <h1>Sign Up</h1>
         <form>
-            <div className="mb-3">
-                <label htmlFor="exampleInputEmail1" className="form-label">Адрес электронной почты</label>
-                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                <div id="emailHelp" className="form-text">Мы никогда никому не передадим вашу электронную почту.</div>
+        <br/>
+        <div className="mb-3">
+                <label htmlFor="inputName" className="form-label">Name</label>
+                <input type="text" className="form-control" id="inputName"/>
             </div>
             <div className="mb-3">
-                <label htmlFor="exampleInputPassword1" className="form-label">Пароль</label>
-                <input type="password" className="form-control" id="exampleInputPassword1"/>
+                <label htmlFor="inputEmail" className="form-label">E-mail</label>
+                <input type="email" className="form-control" id="inputEmail" aria-describedby="emailHelp"/>
+                <div id="emailHelp" className="form-text">We never share your E-mail to anyone else.</div>
+            </div>
+            <div className="mb-3">
+                <label htmlFor="inputPassword" className="form-label">Password</label>
+                <input type="password" className="form-control" id="inputPassword"/>
+            </div>
+            <div className="mb-3">
+                <label htmlFor="inputRepeatPassword" className="form-label">Confirm Password</label>
+                <input type="password" className="form-control" id="inputRepeatPassword"/>
             </div>
             <div className="mb-3 form-check">
                 <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-                <label className="form-check-label" htmlFor="exampleCheck1">Проверить меня</label>
+                <label className="form-check-label" htmlFor="exampleCheck1">Check box :)</label>
             </div>
-            <button type="submit" className="btn btn-primary">Отправить</button>
+            <br/>
+            <button type="submit" className="btn btn-primary">Create Account</button>
         </form>
     </div>
   );
