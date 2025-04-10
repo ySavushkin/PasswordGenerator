bd434c4523c3dcf977ed2be78444fd6515a2c38c
Nastik-14 <Anastasiya.14.trambo@icoud.com>
2025-04-10 22:27:37 +0300

    [frontend] Setup Linter

--- frontend/password-generator/src/pages/authorization/components/register/Register.tsx
+++ frontend/password-generator/src/pages/authorization/components/register/Register.tsx
@@ -55,7 +55,7 @@ const RegistrationForm: React.FC = () => {
                     </div>
                     <div className="field">
                         <svg height="24" width="32" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd" clip-rule="evenodd" d="M3.75 5.25L3 6V18L3.75 18.75H20.25L21 18V6L20.25 5.25H3.75ZM4.5 7.6955V17.25H19.5V7.69525L11.9999 14.5136L4.5 7.6955ZM18.3099 6.75H5.68986L11.9999 12.4864L18.3099 6.75Z" fill="#ffffff"/>
+                            <path fillRule="evenodd" clipRule="evenodd" d="M3.75 5.25L3 6V18L3.75 18.75H20.25L21 18V6L20.25 5.25H3.75ZM4.5 7.6955V17.25H19.5V7.69525L11.9999 14.5136L4.5 7.6955ZM18.3099 6.75H5.68986L11.9999 12.4864L18.3099 6.75Z" fill="#ffffff"/>
                         </svg>
                         <input
                             type="email"
