f9fb2585ef867f611d10bede4dbd91429145c60e
Nastik-14 <Anastasiya.14.trambo@icoud.com>
2025-04-10 23:39:09 +0300

    [frontend] Simple setup for Pass Generator page

--- frontend/password-generator/src/pages/password-generator/PasswordGenerator.tsx
+++ frontend/password-generator/src/pages/password-generator/PasswordGenerator.tsx
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import RangeSlider from "./components/RangeSlider";
 
 const PasswordGenerator: React.FC = () => {
 
+    const [rangeValue, setRangeValue] = useState<number>(16);
+
+    const handleDataFromRange = (data: number) => {
+        setRangeValue(data);
+    };
 
     return (
-        <RangeSlider/>
+        <>
+            <label htmlFor="customRange" className="form-label text-white">
+                Password Length <strong></strong>
+            </label>
+
+            <RangeSlider setCurrentValue={handleDataFromRange}/>
+
+            <input type="number" className="form-control" id="exampleInputText" value={rangeValue}/>
+            
+            <div className="checkbox text-white">
+                <label><input type="checkbox" value=""/>Uppercase</label>
+            </div>
+            <div className="checkbox text-white">
+                <label><input type="checkbox" value=""/>Lowercase</label>
+            </div>
+            <div className="checkbox text-white disabled">
+                <label><input type="checkbox" value="" disabled/>Numbers</label>
+            </div>
+            <div className="checkbox text-white disabled">
+                <label><input type="checkbox" value="" disabled/>Symbols</label>
+            </div>
+        </>
     );
 };
 
