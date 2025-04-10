5bb0296d21372883c0c36975c50fe8c594836e96
Nastik-14 <Anastasiya.14.trambo@icoud.com>
2025-04-10 21:39:18 +0300

    [frontend] Created Password Generator

---
+++ frontend/password-generator/src/pages/password-generator/PasswordGenerator.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import RangeSlider from "./components/RangeSlider";
+
+const PasswordGenerator: React.FC = () => {
+
+
+    return (
+        <RangeSlider/>
+    );
+};
+
+export default PasswordGenerator;
--- frontend/password-generator/src/router/AppRoutes.tsx
+++ frontend/password-generator/src/router/AppRoutes.tsx
@@ -2,10 +2,12 @@ import Register from "../pages/authorization/components/register/Register";
 import LoginPage from "../pages/authorization/components/login/Login";
 import { RouterConfig } from "./RouterConfig";
 import { Navigate } from "react-router-dom";
+import PasswordGenerator from "../pages/password-generator/PasswordGenerator";
 
 const appRoutes: RouterConfig[] = [
     { path: "auth/register", element: <Register />, title: "Register" },
     { path: "auth/login", element: <LoginPage />, title: "Log In" },
+    { path: "password-generator", element: <PasswordGenerator />, title: "Password Generator" },
     { path: "*", element: <Navigate to="/auth/register" replace/>, title: "Authorization"}
 ];
 
