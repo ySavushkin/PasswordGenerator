9b5f21e5012c55f34f05a7ff8fa111af69ac7a03
Nastik-14 <Anastasiya.14.trambo@icloud.com>
2025-06-03 13:52:19 +0300

    Removing the "to generator" button

--- frontend/password-generator/src/App.tsx
+++ frontend/password-generator/src/App.tsx
@@ -1,13 +1,12 @@
-import { BrowserRouter, Link } from 'react-router-dom';
+import { BrowserRouter} from 'react-router-dom';
 import './App.css';
 import AppRouter from './router/AppRouter';
-import { RoutePaths } from './router/RoutePaths';
+//import { RoutePaths } from './router/RoutePaths';
 
 function App() {
     return (
         <BrowserRouter>
             <AppRouter/>
-            <button type="button" className="btn btn-success"><Link className="text-white" to={RoutePaths.PASSWORD_GENERATOR}> TO GENERATOR </Link></button>
         </BrowserRouter>
     );
 }
